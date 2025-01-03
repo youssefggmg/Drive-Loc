99ae6a6f2c121d3d38f287432106bb4eebb0211f
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-01-03 10:27:27 +0100

    added a function to make the log in in the controller file and fineshed that sign up prossis

--- controllers/login.php
+++ controllers/login.php
@@ -2,12 +2,12 @@
 include "../Class/signup.php";
 include "../instance/instace.php";
 $signup = new AUTH($pdo);
-$result=$signup->signup();
+$result=$signup->login();
 if ($result["status"]=0) {
-    header("location: ../sign.php?message=".$result["error"]);
+    header("location: ../index.php?message=".$result["error"]);
 }
 if ($result["status"]=1) {
-    setcookie("userID", $result['index'], time() + 3600,"/");
+    setcookie("userID", $result['ID'], time() + 3600,"/");
     setcookie("roleID", $result['role'], time() + 3600,"/");
     header("location: ../client/home.php");
 }
